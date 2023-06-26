2553434d139742d2a0138adc4c75a1c9e431bf6c
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-06-26 13:20:29 +0300

    fix: Исправлены ссылки на проекты портфолио

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -8,15 +8,15 @@ const Portfolio = () => {
       <HeadingLevel5 name="Портфолио" className="portfolio__title"/>
       <ul className="portfolio__projects">
         <li className="portfolio__projects-item">
-          <Link className="portfolio__project-link" target="_blank" to="https://www.youtube.com/">
+          <Link className="portfolio__project-link" target="_blank" to="https://github.com/TIPDMR/russian-travel">
             Статичный сайт <i className="portfolio__icon-arrow"></i>
           </Link></li>
         <li className="portfolio__projects-item">
-          <Link className="portfolio__project-link" target="_blank" to="https://www.youtube.com/">
+          <Link className="portfolio__project-link" target="_blank" to="https://github.com/TIPDMR/mesto-react">
             Адаптивный сайт <i className="portfolio__icon-arrow"></i>
           </Link></li>
         <li className="portfolio__projects-item">
-          <Link className="portfolio__project-link" target="_blank" to="https://www.youtube.com/">
+          <Link className="portfolio__project-link" target="_blank" to="https://github.com/yandex-praktikum/react-mesto-api-full-gha">
             Одностраничное приложение <i className="portfolio__icon-arrow"></i></Link>
         </li>
       </ul>
