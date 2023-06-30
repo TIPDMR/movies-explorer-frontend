8623b3023eb357c2b56a6fcc7bc0ae3a4d388bf8
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-06-30 23:29:19 +0300

    fix: Исправил отображение карточек на малых разрешениях

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,9 @@ import image from '../images/movies/test.jpg';
 export const MOVIES_LIST = [
   { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
   { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: true },
-  // { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
-  // { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
-  // { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: true },
+  { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
+  { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
+  { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: true },
   // { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
   // { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: false },
   // { name: "33 слова о дизайне", image: image, time: "1ч42м", favorite: true },
