a1f8ad0ff48389dbf2cb8d39a562ba7ad55fb1a6
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-07-10 13:46:31 +0300

    fix: Изменен текст и фото в разделе About.Me

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HeadingLevel2 from "../../HeadingLevel/HeadingLevel2/HeadingLevel2";
 import HeadingLevel3 from "../../HeadingLevel/HeadingLevel3/HeadingLevel3";
 import HeadingLevel4 from "../../HeadingLevel/HeadingLevel4/HeadingLevel4";
-import photo from "../../../images/photo.svg";
+import photo from "../../../images/photo.jpg";
 
 const AboutMe = () => {
   return (
@@ -10,13 +10,14 @@ const AboutMe = () => {
       <HeadingLevel2 name="Студент"/>
       <article className="about-me__article">
         <div className="about-me__container">
-          <HeadingLevel3 name="Виталий" className="about-me__heading-level-3"/>
+          <HeadingLevel3 name="Дмитрий" className="about-me__heading-level-3"/>
           <HeadingLevel4 name="Фронтенд-разработчик, 35 лет" className="about-me__heading-level-4"/>
-          <p className="about-me__text">Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ.
-            У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать
-            музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить.
-            С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;. После того, как прошёл курс
-            по&nbsp;веб-разработке, начал заниматься фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.
+          <p className="about-me__text">Разработкой веб-приложений занимаюсь с 2012 года, когда вместе с друзьями открыли первый приватный игровой сервер и нам потребовался сайт. С
+            тех пор я развивался в различных областях программирования. Писал скрипты на Python для обработки данных, настраивал серверы для защиты от DDoS атак и искал решения для
+            разных задач, которые возникали в процессе работы. Однако, больше всего меня привлекало создание сайтов и панелей управления. Работал в серой нише, создавая панели на
+            Yii2. Недавно принял решение выбрать профессию веб-разработчика в качестве основного направления для своего личного развития. Успешно завершил онлайн-курс
+            "Web-разработчик" в Яндекс.Практикуме, где получил множество новых знаний и познакомился с JavaScript библиотекой React. Планирую продолжать свое развитие в этом
+            направлении.
           </p>
           <div className="about-me__links">
             <a className="about-me__link" href="https://github.com/TIPDMR">Github</a>
